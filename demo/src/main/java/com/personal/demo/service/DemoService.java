9a0648b8685be33ac1b924b75ae9c56b213296d6
Prasanth <35705202+CPrasanth01@users.noreply.github.com>
2020-01-23 23:14:02 +0530

    Adding Loggers using log4j2.properties

--- demo/src/main/java/com/personal/demo/service/DemoService.java
+++ demo/src/main/java/com/personal/demo/service/DemoService.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import com.personal.demo.repository.DemoRepository;
 
 @Service
 public class DemoService {
+	private static final Logger log = LogManager.getLogger(DemoService.class);
 
 	@Autowired
 	DemoRepository demoRepo;
@@ -25,11 +28,10 @@ public class DemoService {
 		return (List<Demo>) demoRepo.findAll();
 	}
 
-	public List<Demo> getDetailsByLastName(String lastName){
+	public List<Demo> getDetailsByLastName(String lastName) {
 		return demoRepo.findByLastName(lastName);
 	}
-	
-	
+
 // Scheduler - Cron
 //	"0 0 * * * *" = the top of every hour of every day.
 //	"*/10 * * * * *" = every ten seconds.
@@ -38,9 +40,9 @@ public class DemoService {
 //	"0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30, 10:00 and 10:30 every day.
 //	"0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays
 //	"0 0 0 25 12 ?" = every Christmas Day at midnight
-	
-	@Scheduled(cron ="*/15 * * * * *")
+
+	@Scheduled(cron = "*/15 * * * * *")
 	public void runOnInterval() {
-		System.out.println("Refreshing at : " +new Date());
+		log.info("Refreshing at : " + new Date());
 	}
 }
